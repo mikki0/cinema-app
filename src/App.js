45cc779dc089e5dd6adbf070074e72c9af177fc7
mikolaj <mikolajsve@gmail.com>
2020-04-06 23:22:35 +0200

    fix fetch error

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-
 import './App.scss';
 import Header from './components/Header/Header';
 import Searchbar from './components/Header/Searchbar/Searchbar';
 import Aside from './components/Aside/Aside';
-// import Main from './components/Main/Main';
-import Sign from './components/Header/Sign/Sign';
+import Main from './components/Main/Main';
 
 class App extends React.Component {
   state = {
-    searchedMovie: {
-      Search: null,
-    },
+    searchedMovie: [],
+    targetValue: '',
   };
 
   searchMovies = (event) => {
     event.preventDefault();
     const searchText = event.target[0].value;
     this.setState({ searchedMovie: [] });
+    this.setState({ targetValue: searchText });
     fetch('http://www.omdbapi.com/?apikey=6f576247&s=' + searchText)
       .then((response) => response.json())
       .then((response) => {
-        this.setState({ searchedMovie: response });
+        this.setState({ searchedMovie: response.Search });
       });
 
     event.target.reset();
@@ -29,19 +27,22 @@ class App extends React.Component {
 
   render() {
     // zwraca najpierw undefined a dopiero potem tablicę z filmami
-    console.log(this.state.searchedMovie.Search);
+    console.log(this.state.searchedMovie);
     return (
       <>
         <Header>
           <Searchbar submitFn={this.searchMovies} />
-          <Sign />
         </Header>
         <div className="wrap">
           <Aside />
-          {/* 
-          
-          Main.js do wyrenderowania w tym miejscu -          
-          */}
+          {this.state.searchedMovie[0] ? (
+            <Main
+              searchedMovie={this.state.searchedMovie}
+              targetValue={this.state.targetValue}
+            />
+          ) : (
+            ''
+          )}
         </div>
       </>
     );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,27 +3,16 @@ import styles from './Main.module.scss';
 
 const Main = (props) => (
   <main>
-    <h2 className={styles.title}>Searched movies</h2>
-    <section
-      className={props.searchedMovie.Search[0].Title ? styles.movie : ''}>
-      <div
-        className={
-          props.searchedMovie.Search[0].imdbRating ? styles.rating : ''
-        }>
-        {props.searchedMovie.Search[0].imdbRating}
-      </div>
-      <img
-        className={
-          props.searchedMovie.Search[0].Poster
-            ? styles.movieImage
-            : styles.displayNone
-        }
-        alt="Poster"
-        src={props.searchedMovie.Search[0].Poster}
-      />
-      <h4 className="movieTitle">{props.searchedMovie.Search[0].Title}</h4>
-      <p className="genre">{props.searchedMovie.Search[0].Genre}</p>
-    </section>
+    <h2 className={styles.title}>
+      Searched movies for:<span> {props.targetValue}</span>
+    </h2>
+    {props.searchedMovie.map((item) => (
+      <section className={styles.movie}>
+        <img className={styles.movieImage} alt="Poster" src={item.Poster} />
+        <h4 className={styles.movieTitle}>{item.Title}</h4>
+        <p className={styles.year}>{item.Year}</p>
+      </section>
+    ))}
   </main>
 );
 
